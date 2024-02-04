8435c58ccf54c20971a60126798a73055a229e4b
sagarbhmr@gmail.com <sagarbhr@gmail.com>
2024-02-05 00:26:14 +0530

    filtered events with server side and client side data fetching

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,12 @@
 import { Fragment } from "react";
 import { useRouter } from "next/router";
 import EventList from "@/components/events/event-list";
-import { getAllEvents } from "@/dummy-data";
+import { getAllEvents } from "@/helpers/api-util";
 import EventsSearch from '@/components/events/events-search';
 
-function AllEventsPage(){
+function AllEventsPage(props){
     const router = useRouter();
-    const events = getAllEvents();
+    const { events } = props;
 
     function findEventsHandler(year, month){
         const fullPath= `/events/${year}/${month}`;
@@ -19,6 +19,18 @@ function AllEventsPage(){
                 <EventList items={events}/>
             </Fragment>
     )  
+} 
+
+export async function getStaticProps(){
+    const events = await getAllEvents();
+
+    return {
+        props:{
+            events: events
+        },
+        revalidate: 60
+    }
+
 }
 
 export default AllEventsPage;
